7b963d404d9029fb2513219669c987d07179d1d2
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-23 22:15:24 +0300

    create FormatterFactory as Singleton using approach Enum Singleton

---
+++ src/main/java/com/javajungle/calculator/formatter/FormatterFactory.java
@@ -0,0 +1,38 @@
+package com.javajungle.calculator.formatter;
+
+
+import com.javajungle.calculator.formatter.supplier.FormatterSupplier;
+import com.javajungle.calculator.formatter.supplier.UAFormatterSupplier;
+import com.javajungle.calculator.formatter.supplier.USFormatterSupplier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum FormatterFactory {
+    INSTANCE();
+    private final Map<String, FormatterSupplier> suppliers;
+
+    FormatterFactory() {
+        suppliers = new HashMap<>();
+        register(new UAFormatterSupplier());
+        register(new USFormatterSupplier());
+    }
+
+    public static FormatterFactory getInstance() {
+        return INSTANCE;
+    }
+
+    private void register(FormatterSupplier supplier) {
+        suppliers.put(supplier.getName(), supplier);
+
+    }
+
+    public Formatter get(String name){
+        if(suppliers.containsKey(name)){
+            return suppliers.get(name).get();
+        }
+        throw new IllegalArgumentException("FormatterFactory with name '" + name + "' is not registered!");
+    }
+
+
+}
