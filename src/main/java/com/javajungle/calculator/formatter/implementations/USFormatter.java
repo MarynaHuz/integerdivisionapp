6b28b57018b0ba846ac1f92afb2acd609440f3b7
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-25 15:41:12 +0300

    create formatters

---
+++ src/main/java/com/javajungle/calculator/formatter/implementations/USFormatter.java
@@ -0,0 +1,14 @@
+package com.javajungle.calculator.formatter.implementations;
+
+import com.javajungle.calculator.formatter.Formatter;
+import com.javajungle.calculator.model.Result;
+
+public class USFormatter implements Formatter {
+
+
+    @Override
+    public String format(Result result) {
+
+        return String.format("%s", result);
+    }
+}
