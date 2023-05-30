fe9c26852834ddd7e3a7c3504fc8f1a80620163d
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-30 16:35:48 +0300

    present the class Result like Record

--- src/main/java/com/javajungle/calculator/model/Result.java
+++ src/main/java/com/javajungle/calculator/model/Result.java
@@ -3,40 +3,7 @@ package com.javajungle.calculator.model;
 import java.util.List;
 import java.util.Objects;
 
-public class Result {
-    private final int dividend;
-    private final int divisor;
-    private final int quotient;
-    private final int reminder;
-    private final List<DivisionStep> divisionSteps;
-
-    public Result(int dividend, int divisor, int quotient, int reminder, List<DivisionStep> divisionSteps) {
-        this.dividend = dividend;
-        this.divisor = divisor;
-        this.quotient = quotient;
-        this.reminder = reminder;
-        this.divisionSteps = divisionSteps;
-    }
-
-    public int getDividend() {
-        return dividend;
-    }
-
-    public int getDivisor() {
-        return divisor;
-    }
-
-    public int getQuotient() {
-        return quotient;
-    }
-
-    public int getReminder() {
-        return reminder;
-    }
-
-    public List<DivisionStep> getDivisionSteps() {
-        return divisionSteps;
-    }
+public record Result(int dividend, int divisor, int quotient, int reminder, List<DivisionStep> divisionSteps) {
 
     @Override
     public boolean equals(Object o) {
@@ -46,18 +13,7 @@ public class Result {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(dividend, divisor, quotient, reminder, divisionSteps);
-    }
-
-    @Override
     public String toString() {
-        return "Result{" +
-                "dividend=" + dividend +
-                ", divisor=" + divisor +
-                ", quotient=" + quotient +
-                ", reminder=" + reminder +
-                ", divisionSteps=" + divisionSteps +
-                '}';
+        return String.format("%d,%d,%d,%d,%s", dividend, divisor, quotient, reminder, divisionSteps);
     }
 }
