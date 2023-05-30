a86bfa5e1b9fd1a89109d9f98e26ee008ad1fa7d
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-30 16:37:14 +0300

    present the class DivisionStep like Record, add new fields, and update current fields

--- src/main/java/com/javajungle/calculator/model/DivisionStep.java
+++ src/main/java/com/javajungle/calculator/model/DivisionStep.java
@@ -1,42 +1,16 @@
 package com.javajungle.calculator.model;
 
-import java.util.Objects;
-
-public class DivisionStep {
-
-    private final int mod;
-    private final int multiplyResult;
-
-    public DivisionStep(int mod, int multiplyResult) {
-        this.mod = mod;
-        this.multiplyResult = multiplyResult;
-    }
-
-    public int getMultiplyResult() {
-        return multiplyResult;
-    }
-
-    public int getMod() {
-        return mod;
-    }
+public record DivisionStep(int difference, int subtrahend, int remainderAfterEachStep, int quotientDigit) {
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof DivisionStep that)) return false;
-        return mod == that.mod && multiplyResult == that.multiplyResult;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(mod, multiplyResult);
+        return difference == that.difference && subtrahend == that.subtrahend && remainderAfterEachStep == that.remainderAfterEachStep && quotientDigit == that.quotientDigit;
     }
 
     @Override
     public String toString() {
-        return "{" +
-                 mod + ","+
-                 multiplyResult +
-                '}';
+        return String.format("%d,%d,%d,%d", difference, subtrahend, remainderAfterEachStep, quotientDigit);
     }
 }
